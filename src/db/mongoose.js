0ecc1538e5159526dc23d6a393b3c5e29a7e1328
AarushMahajan <aarushmahajan15@gmail.com>
2022-08-07 22:27:34 +0530

    added mongodb url

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://127.0.0.1:27017/ticket-manager';
+const url = 'mongodb+srv://aarush:qwertyuiop@cluster0.ti0qmvd.mongodb.net/ticket-manager';
 
 mongoose.connect(url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true 
-    // useFindAndModify: false
+    useUnifiedTopology: true
 })
